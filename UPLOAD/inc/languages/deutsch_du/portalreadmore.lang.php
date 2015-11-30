0c3668d94d605b71bab39210cb0d358d98f646b9
Sven <svp1702@freenet.de>
2015-11-30 18:46:40 +0100

    lang file updates

--- UPLOAD/inc/languages/deutsch_du/portalreadmore.lang.php
+++ UPLOAD/inc/languages/deutsch_du/portalreadmore.lang.php
@@ -1,1 +1,1 @@
-<?php/** PortalReadMore - 1.1 Language File German (informal)* Licensed under GNU/GPL v3*/$l['portalreadmore_readmore'] = "Zeige mehr...";$l['portalreadmore_close'] = "Schließen";?>
+<?php/** * PortalReadMore - 1.1 Language File German (informal) * Licensed under GNU/GPL v3 *  */$l['portalreadmore_readmore'] = "Zeige mehr...";$l['portalreadmore_close'] = "Schließen";
--- UPLOAD/inc/languages/deutsch_sie/portalreadmore.lang.php
+++ UPLOAD/inc/languages/deutsch_sie/portalreadmore.lang.php
@@ -1,1 +1,1 @@
-<?php/** PortalReadMore - 1.1 Language File German (formal)* Licensed under GNU/GPL v3*/$l['portalreadmore_readmore'] = "Zeige mehr...";$l['portalreadmore_close'] = "Schließen";?>
+<?php/** * PortalReadMore - 1.1 Language File German (formal) * Licensed under GNU/GPL v3 *  */$l['portalreadmore_readmore'] = "Zeige mehr...";$l['portalreadmore_close'] = "Schließen";
